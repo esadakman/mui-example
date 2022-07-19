9c44c2533c187e1f2853b6ec1a3219fe102fe31b
esadakman <esadd26@gmail.com>
2022-07-19 17:13:02 +0300

    v3

--- src/components/AppPagination.jsx
+++ src/components/AppPagination.jsx
@@ -1,11 +1,38 @@
 import { Pagination } from "@mui/material";
 import React from "react";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // position: "fixed",
+    bottom: 0,
+    zIndex: 4,
+    // backgroundColor: "green",
+    padding: "10px 80px",
+    color: "white",
+    width: "100%",
+  },
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    coloe: "white",
+  },
+}));
 
 const AppPagination = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <div>
-        <Pagination />
+    <div className={classes.container}>
+      <div className={classes.root}>
+        <Pagination
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+          variant="outlined"
+          count={3}
+        />
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,37 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
+// import * as React from "react";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
+// import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import {
-  Button,
-  CardActionArea,
-  CardActions,
+  // Button,
+  // CardActionArea,
+  // CardActions,
   Grid,
   Container,
 } from "@mui/material";
-import data from "../data";
+// import data from "../data";
 import AppPagination from "../components/AppPagination";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
   // const [courses, setCourse] = React.useState(data);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
 
   return (
     <Container>
@@ -28,11 +45,7 @@ export default function Home() {
         COURSES
       </Typography>
       <Grid container spacing={2} justifyContent="center" alignItems="center">
-        <AppPagination />
-        {data.map((card) => {
-          const { id, name, text, img } = card;
-          return (
-            <Grid item key={id} xs={12} sm={6} md={4}>
+        {/* <Grid item key={id} xs={12} sm={6} md={4}>
               <Card>
                 <CardActionArea>
                   <CardMedia component="img" image={img} alt={name} />
@@ -51,10 +64,35 @@ export default function Home() {
                   </Button>
                 </CardActions>
               </Card>
-            </Grid>
-          );
-        })}
+            </Grid> */}
       </Grid>
+      <AppPagination />
     </Container>
   );
 }
+
+// {data.map((card) => {
+//         const { id, name, text, img } = card;
+//         return (
+//           <Grid item key={id} xs={12} sm={6} md={4}>
+//             <Card>
+//               <CardActionArea>
+//                 <CardMedia component="img" image={img} alt={name} />
+//                 <CardContent>
+//                   <Typography gutterBottom variant="h5" component="div">
+//                     {name}
+//                   </Typography>
+//                   <Typography variant="body2" color="text.secondary">
+//                     {text}
+//                   </Typography>
+//                 </CardContent>
+//               </CardActionArea>
+//               <CardActions>
+//                 <Button size="small" color="primary">
+//                   Share
+//                 </Button>
+//               </CardActions>
+//             </Card>
+//           </Grid>
+//         );
+//       })}
